0aaf41996a2602e0573eeb3ce5bc2fcd3009366a
VoladorSapo <voladorsapo@gmail.com>
2023-04-12 01:53:46 +0200

    principio logica fonos + finalmente el archivo random oficial del juego

--- AtracaJuego/Assets/Scenes/Protipo Assets/Scripts/Text/DialogEvent.cs
+++ AtracaJuego/Assets/Scenes/Protipo Assets/Scripts/Text/DialogEvent.cs
@@ -8,13 +8,18 @@ public class DialogEvent : MonoBehaviour
   [SerializeField]  private TMP_Text _textbox;
     [SerializeField] GameDialogController _GDC;
     [SerializeField] DialogueController _DC;
-
+    [SerializeField] Camera _cutscenecamera;
+    [SerializeField] Camera _maincamera;
+    [SerializeField] Animator _fondoanimator;
 
     private void Awake()
     {
         _textbox = GetComponentInChildren<TMP_Text>();
         _GDC = GetComponentInChildren<GameDialogController>();
         _DC = GetComponentInChildren<DialogueController>();
+        _maincamera = GameObject.Find("Main Camera").GetComponent<Camera>();
+        _cutscenecamera = GameObject.Find("CutsceneCamera").GetComponent<Camera>();
+        _fondoanimator = GameObject.Find("CutsceneFondo").GetComponent<Animator>();
 
     }
 
@@ -52,6 +57,23 @@ public class DialogEvent : MonoBehaviour
                     {
                         _DC.setEmote(int.Parse(eventoarray[2]));
                     }
+                }
+                break;
+            case "SetFondo":
+                if (_DC)
+                {
+                    _fondoanimator.SetInteger("Fondo",int.Parse(eventoarray[1]));
+                    if(int.Parse(eventoarray[1]) == 0)
+                    {
+                        _cutscenecamera.enabled = false;
+                        _maincamera.enabled = true;
+                    }
+                    else
+                    {
+                        _cutscenecamera.enabled = true;
+                        _maincamera.enabled = false;
+                    }
+                    
                 }
                 break;
         }
