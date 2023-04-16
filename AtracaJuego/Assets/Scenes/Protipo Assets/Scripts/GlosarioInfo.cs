7f77095cd07ae3390c6094ecf45468ffe9d0a271
VoladorSapo <voladorsapo@gmail.com>
2023-04-16 20:24:33 +0200

    Espero no romper nada

---
+++ AtracaJuego/Assets/Scenes/Protipo Assets/Scripts/GlosarioInfo.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GlosarioInfo : MonoBehaviour
+{
+    bool[] combinacionespermanentes;
+    GlosarioController glosario;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        combinacionespermanentes = new bool[25];
+        combinacionespermanentes[0] = true;
+        DontDestroyOnLoad(this);
+        SceneManager.sceneLoaded += ChangedActiveScene;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    void ChangedActiveScene(Scene scene, LoadSceneMode mode)
+    {
+        if (scene.buildIndex != 0)
+        {
+            glosario = GameObject.Find("GlosarioController").GetComponent<GlosarioController>();
+            glosario.combinaciones = combinacionespermanentes;
+        }
+    }
+}
