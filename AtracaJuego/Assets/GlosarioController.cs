9ec220dd04d77c6e78f4c3bdd44552c686de12ae
VoladorSapo <voladorsapo@gmail.com>
2023-04-17 14:43:26 +0200

    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque quis interdum nibh, nec convallis leo. Nam sagittis dignissim justo eu mattis. In vitae massa a libero auctor lacinia. Duis vehicula condimentum sodales. Etiam pretium tempus eleifend. Donec at sagittis massa. Nullam id tristique nulla, a elementum purus. Donec sagittis, risus eu cursus posuere, nisl leo tincidunt nulla, id auctor enim sapien eget risus. Etiam commodo, metus id condimentum dignissim, lectus magna luctus tellus, a convallis sem mauris nec purus.

--- AtracaJuego/Assets/GlosarioController.cs
+++ AtracaJuego/Assets/GlosarioController.cs
@@ -69,6 +69,14 @@ public class GlosarioController : MonoBehaviour
             _info.combinacionespermanentes = combinaciones;
         }
     }
+    /* Lista combinaciones
+     * 1 = Explosion;    2 = Derretir Hielo;    3 = Congelar Agua;    4 = Empujar Hielo;    5 = Derretir Cubo;    
+     * 6 = Empujar Gas;    7 = Crear Gasolina;   8 = Cargar Gas;    9 = Cargar Gasolina;    10 = Electrocutar Agua;
+     * 11 = Quemar Gasolina;    12 = Congelar Gasolina;    13 = Pinchos;    14 = Pinchos Gasolina;    15 = Evaporar Agua;
+     * 16 = Derretir Gasolina;  17 = Cargar Iowa;   18 = Gas Sobre Fuego; 19 = Hielo Sobre Fuego
+     *  
+     * 
+     */
     public void ChangeGlosario(int baseeffect, int neweffect,bool iceCube, Vector3 pos)
     {
         print(baseeffect + " " + neweffect +" glosario");
@@ -78,51 +86,30 @@ public class GlosarioController : MonoBehaviour
                 switch (neweffect)
                 {
                     case 4:
+                        newEntry(18);
                         break;
                     case 5:
+                        newEntry(7);
 
                         break;
                     case 6:
+                        newEntry(8);
 
                         break;
                 }
                 break;
             case 2: //Fire
-                if (iceCube)
-                {
-                    switch (neweffect)
-                    {
-                        case 1:
-                            newEntry(1);
-                            break;
-                        case 5:
-
-                            break;
-                        case 7:
-
-                            break;
-                        case 8:
-
-                            break;
-                        case 9:
-
-                            break;
-                        case 10:
-
-                            break;
-                    }
-                }
-                break;
+                newEntry(5);                break;
             case 3: //Push
-                if (iceCube)
+                if (!iceCube)
                 {
                     switch (neweffect)
                     {
                         case 5:
-
+                            newEntry(2);
                             break;
                         case 9:
-
+                            newEntry(16);
                             break;
                       
                     }
@@ -132,13 +119,13 @@ public class GlosarioController : MonoBehaviour
                 switch (neweffect)
                 {
                     case 1:
-
+                        newEntry(7);
                         break;
                     case 3:
-
+                        newEntry(12);
                         break;
                     case 4:
-
+                        newEntry(19);
                         break;
                 }
                 break;
@@ -146,16 +133,17 @@ public class GlosarioController : MonoBehaviour
                 switch (neweffect)
                 {
                     case 1:
-
+                        newEntry(8);
                         break;
                     case 2:
-
+                        newEntry(10);
                         break;
                     case 3:
-
+                        newEntry(9);
                         break;
                 }
                 break;
         }
     }
+
 }
