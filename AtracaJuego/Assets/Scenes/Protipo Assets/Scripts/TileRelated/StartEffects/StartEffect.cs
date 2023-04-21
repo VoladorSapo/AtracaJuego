0d56ae1b59c660cca95459ab3d615180959a99dd
Rolexless <pepinero111@gmail.com>
2023-04-21 21:39:16 +0200

    Nunca te Rindas

--- AtracaJuego/Assets/Scenes/Protipo Assets/Scripts/TileRelated/StartEffects/StartEffect.cs
+++ AtracaJuego/Assets/Scenes/Protipo Assets/Scripts/TileRelated/StartEffects/StartEffect.cs
@@ -14,6 +14,6 @@ public class StartEffect : MonoBehaviour
             {
                 sws.Starting();
             }
-}
+        }
     }
 }
--- AtracaJuego/Assets/Scenes/Protipo Assets/Scripts/TileRelated/StartEffects/StartWet.cs
+++ AtracaJuego/Assets/Scenes/Protipo Assets/Scripts/TileRelated/StartEffects/StartWet.cs
@@ -10,7 +10,7 @@ public class StartWet : MonoBehaviour
         GC=GameObject.Find("Grid").GetComponent<GridController>();
     }
     public void Starting(){
-        print(GC); print(GC.grid);
+    
     Vector3Int posInGrid=GC.grid.WorldToCell(transform.position) - new Vector3Int(GC.ogx,GC.ogy,0);
     GC.tiles[posInGrid.x,posInGrid.y].trySetEffect(2); //2 es mojado
     GC.tiles[posInGrid.x,posInGrid.y].addEffect(6,false,0,-1); //En addeffect es 6 (lioso pero ya es tarde para cambiarlo por el momento)
